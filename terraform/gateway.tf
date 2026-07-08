c68df7a36b2b0b4b6e89b7d584a0fbfe003c2763
Taufik Khan <taufik4051@gmail.com>
2026-07-08 13:46:59 +0530

    changes for ngnix

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -1,3 +1,11 @@
+resource "null_resource" "gateway_api_crds" {
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --region ap-south-1 --name todo-eks && kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.3.0/standard-install.yaml"
+  }
+
+  depends_on = [module.eks]
+}
+
 resource "helm_release" "nginx_gateway" {
   name             = "ngf"
   repository       = "oci://ghcr.io/nginx/charts"
